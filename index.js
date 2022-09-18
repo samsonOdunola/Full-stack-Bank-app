3dd07f683e25bfad6347a49d7154744d3ea3b27a
samsonOdunola <odunolasamson@yahoo.com>
2022-09-18 20:09:28 +0100

    more updates

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 require("dotenv").config();
-App.get("*", (req, res) => {
+App.get("/*", (req, res) => {
   res.sendFile(__dirname + "/build/index.html");
 });
 App.use(express.static("./build"));
 App.use(express.urlencoded({ extended: true, limit: "50mb" }));
 App.use(express.json({ limit: "50mb" }));
 App.use(cors());
-const port = process.env.PROP || 3000;
+const port = process.env.PORT || 3000;
 const URI = process.env.URI;
 const userRouter = require("./Routes/user.route");
 App.use("/user", userRouter);
-// App.listen(port, () => {
-//   console.log("Server is running on port " + port);
-// });
+App.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
 
 mongoose.connect(URI, (err) => {
   if (err) {
