03dfb3783101975a7c88c6c5c6881f25a0bedce3
samsonOdunola <odunolasamson@yahoo.com>
2022-09-21 15:50:52 +0100

    update

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ require("dotenv").config();
 const port = process.env.PORT || 3000;
 const URI = process.env.URI;
 const userRouter = require("./Routes/user.route");
-App.use("/user", userRouter);
 
+App.use(express.static("./build"));
 App.get("/*", (req, res) => {
   res.sendFile(__dirname + "/build/index.html");
 });
-App.use(express.static("./build"));
+App.use("/user", userRouter);
+
 App.use(express.urlencoded({ extended: true, limit: "50mb" }));
 App.use(express.json({ limit: "50mb" }));
 App.use(cors());
